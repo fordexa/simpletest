019b84b81af8625ee90a069995e45050244f3213
unknown <isayko.forwork@gmail.com>
2009-02-12 12:07:24 +0200

    add tests suite

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from simpletest.profiles.forms import EditProfileForm
 from django.contrib.auth.decorators import login_required
+from registration.models import RegistrationProfile
 
 @login_required
 def my_profile(request):
+    profile = get_object_or_404(RegistrationProfile, user=request.user)
     return render_to_response('profiles/my.html',
-                              {'user': request.user},
+                              {'user': profile.user},
                               RequestContext(request))
 
 @login_required
